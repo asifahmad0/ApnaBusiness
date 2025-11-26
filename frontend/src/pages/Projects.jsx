33d307d6a7f7c783ea9052b1938c6f2acb6dc6a4
ASIF AHMAD <67062945+asifahmad0@users.noreply.github.com>
2025-11-27 01:48:44 +0530

    Update Projects.jsx

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -62,7 +62,7 @@ export default function ProjectsSection() {
         We Are In All Field
       </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-2 overflow-hidden">
         {videos.map((video) => (
           <HoverVideoCard key={video.id} video={video} />
         ))}
