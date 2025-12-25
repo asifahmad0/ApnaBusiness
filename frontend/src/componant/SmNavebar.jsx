765faf11cf19d63221c60966dc03f5617efb95e9
ASIF AHMAD <67062945+asifahmad0@users.noreply.github.com>
2025-12-25 22:52:36 +0530

    Update SmNavebar.jsx

--- frontend/src/componant/SmNavebar.jsx
+++ frontend/src/componant/SmNavebar.jsx
@@ -9,7 +9,7 @@ function SmNavebar() {
   return (
     <div className={`sm_nave fixed z-[9999] w-[100dvw] h-[60px] flex  lg:flex-row items-center  justify-between px-17  ${isOpen? "h-screen flex-col": "h-[50px] overflow-hidden"} `}>
 
-      <div className="logo font-[monospace] p-1 text-xl lg:text-2xl font-bold text-primery ">Digilink<span className=" text-textColor">tech solution</span></div>
+      <div className="logo font-[monospace] p-1 text-xl lg:text-2xl font-bold text-primery ">Digilink <br><span className=" text-textColor">tech solution</span></div>
       
 
       {/* Menu Links */}
