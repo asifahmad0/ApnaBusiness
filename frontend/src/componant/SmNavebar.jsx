93afb6d286a646d696d41b224a3e0d50ab93b8bb
ASIF AHMAD <67062945+asifahmad0@users.noreply.github.com>
2025-12-29 11:55:11 +0530

    Update SmNavebar.jsx

--- frontend/src/componant/SmNavebar.jsx
+++ frontend/src/componant/SmNavebar.jsx
@@ -7,7 +7,7 @@ function SmNavebar() {
 
 
   return (
-    <div className={`sm_nave fixed z-[9999] w-[100dvw] h-[60px] flex  lg:flex-row items-center  justify-between px-17  ${isOpen? "h-screen flex-col": "h-[50px] overflow-hidden"} `}>
+    <div className={`sm_nave bg-bg3 fixed z-[9999] w-[100dvw] h-[60px] flex  lg:flex-row items-center  justify-between px-17  ${isOpen? "h-screen flex-col": "h-[50px] overflow-hidden"} `}>
 
       <div className="logo  flex flex-col items-center ">
         <h1 className=" font-[monospace] text-xl lg:text-2xl font-bold text-primery ">STAR FUSION </h1>
